abb7984f12df6913b1abc99c4fc0ad2c2ecae8be
Nuruddin Ashr <uudashr@gmail.com>
2026-05-17 20:55:39 +0700

    unused: dedicated test for exclusion

--- unused/unused_test.go
+++ unused/unused_test.go
@@ -8,6 +8,16 @@ import (
 )
 
 func Test(t *testing.T) {
+	testdata := analysistest.TestData()
+	analysistest.RunWithSuggestedFixes(t, testdata, unused.Analyzer, "a")
+	analysistest.RunWithSuggestedFixes(t, testdata, unused.Analyzer, "agroup")
+	analysistest.RunWithSuggestedFixes(t, testdata, unused.Analyzer, "agroupdoc")
+	analysistest.RunWithSuggestedFixes(t, testdata, unused.Analyzer, "agroupsingle")
+	analysistest.RunWithSuggestedFixes(t, testdata, unused.Analyzer, "b")
+	analysistest.RunWithSuggestedFixes(t, testdata, unused.Analyzer, "d")
+}
+
+func TestExclusion(t *testing.T) {
 	err := unused.Analyzer.Flags.Set("exclude", "c")
 	if err != nil {
 		t.Fatal(err)
@@ -15,10 +25,5 @@ func Test(t *testing.T) {
 
 	testdata := analysistest.TestData()
 	analysistest.RunWithSuggestedFixes(t, testdata, unused.Analyzer, "a")
-	analysistest.RunWithSuggestedFixes(t, testdata, unused.Analyzer, "agroup")
-	analysistest.RunWithSuggestedFixes(t, testdata, unused.Analyzer, "agroupdoc")
-	analysistest.RunWithSuggestedFixes(t, testdata, unused.Analyzer, "agroupsingle")
-	analysistest.RunWithSuggestedFixes(t, testdata, unused.Analyzer, "b")
 	analysistest.RunWithSuggestedFixes(t, testdata, unused.Analyzer, "c")
-	analysistest.RunWithSuggestedFixes(t, testdata, unused.Analyzer, "d")
 }
