9fead0913ec5d0895f1334b44ceb8e8be7880a3f
Nuruddin Ashr <uudashr@gmail.com>
2026-05-19 22:38:36 +0700

    Apply ignore directive properly

--- identical/identical.go
+++ identical/identical.go
@@ -55,7 +55,9 @@ func (r *runner) run(pass *analysis.Pass) (interface{}, error) {
 			return
 		}
 
-		r.debugf("GenDecl: %v specs=%d\n", decl.Tok, len(decl.Specs))
+		if r.debug {
+			fmt.Fprintf(os.Stderr, "GenDecl: %v specs=%d\n", decl.Tok, len(decl.Specs))
+		}
 
 		if decl.Tok != token.TYPE {
 			return
@@ -83,25 +85,21 @@ func (r *runner) run(pass *analysis.Pass) (interface{}, error) {
 			}
 
 			if r.debug {
-				fmt.Fprintln(os.Stderr, "Interface declaration:", ts.Name.Name, ts.Pos(), len(ifaceType.Methods.List))
+				fmt.Fprintln(os.Stderr, "  -> Interface declaration:", ts.Name.Name, ts.Pos(), len(ifaceType.Methods.List))
 
 				for i, field := range ifaceType.Methods.List {
 					switch ft := field.Type.(type) {
 					case *ast.FuncType:
-						fmt.Fprintf(os.Stderr, " [%d] Field: func %s %T %v\n", i, field.Names[0].Name, ft, field.Pos())
+						fmt.Fprintf(os.Stderr, "  [%d] Field: func %s %T %v\n", i, field.Names[0].Name, ft, field.Pos())
 					case *ast.Ident:
-						fmt.Fprintf(os.Stderr, " [%d] Field: iface %s %T %v\n", i, ft.Name, ft, field.Pos())
+						fmt.Fprintf(os.Stderr, "  [%d] Field: iface %s %T %v\n", i, ft.Name, ft, field.Pos())
 					default:
-						fmt.Fprintf(os.Stderr, " [%d] Field: unknown %T\n", i, ft)
+						fmt.Fprintf(os.Stderr, "  [%d] Field: unknown %T\n", i, ft)
 					}
 				}
 			}
 
 			dir := directive.ParseIgnore(ts.Doc)
-			if dir == nil {
-				dir = blockDir
-			}
-
 			if dir != nil && dir.ShouldIgnore(pass.Analyzer.Name) {
 				continue
 			}
